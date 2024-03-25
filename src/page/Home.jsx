1654783772c0a4a4d2e6aed605de22b2aa3cf146
bendinuralif <bendipenthol@gmail.com>
2024-03-26 03:08:38 +0700

    update home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -23,7 +23,7 @@ function HomePage() {
                 PERSAUDARAAN SETIA HATI TERATE
               </h5>
             </a>
-            <p class="text-sm mb-3 font-normal text-gray-700 dark:text-gray-400 md:text-lg text-justify">
+            <p class="text-sm mb-3 p-5 font-normal text-gray-700 dark:text-gray-400 md:text-lg text-justify">
             Didirikan di Madiun pada tahun 1922 oleh Ki Hajar Hardjo Oetomo (1888 – 1952), seorang pahlawan Perintis Kemerdekaan RI, PSHT semula bernama Setia Hati Pemuda Sport Club (SH PSC) yang berbentuk organisasi. Nama ini kemudian menjadi Persaudaraan Setia Hati “Pemuda Sport Club” dan akhirnya diubah menjadi “Persaudaraan Setia Hati Terate” dalam kongres pertama di Madiun, 25 Maret 1951.
         <br /><br />
         Perkembangan PSHT tidak terlepas dari jasa beberapa tokoh yang turut membesarkan PSHT, diantaranya Bpk. RM Soetomo Mangkudjojo; Bpk. Santoso Kartoatmodjo; Bpk. Irsyad; Mas RM. Imam Koesoepangat dan Mas KRT Tarmadji Budi Harsono, SE. Beliau-beliaulah yang meletakkan berbagai dasar dan memperintis pengembangan PSHT yang masih digunakan dan berlaku hingga saat ini. Berkat jasa mereka semua, PSHT sejak lama sudah memiliki AD-ART, mendirikan sebuah yayasan, mengembangkan PSHT dengan membentuk banyak cabang, membangun padepokan sebagai pusat kegiatan PSHT, mendirikan koperasi yang kini akan diperluas dengan melibatkan semua anggota di seluruh cabang, dan makin dikenalnya PSHT melalui berbagai kejuaraan.
