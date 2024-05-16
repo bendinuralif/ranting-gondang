e80cf4be517c2603263bb406e57f5d49eb985881
bendinuralif <bendipenthol@gmail.com>
2024-05-16 12:12:14 +0700

    update

---
+++ src/admin/page/Galeri.jsx/Tambah.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+import LayoutAdmin from "../LayoutAdmin";
+import { addDoc, collection } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"; // Tambahkan impor getDownloadURL
+import app from "../../../lib/firebase/init";
+
+function TambahGaleri() {
+  const [uploadGambar, setUploadGambar] = useState("");
+  const [previewImage, setPreviewImage] = useState(""); // State untuk menyimpan URL priview gambar
+  const [nama, setNama] = useState("");
+  const [tahun, setTahun] = useState("");
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const db = getFirestore(app);
+    try {
+      // Upload gambar ke Firebase Storage
+      const storage = getStorage(app);
+      const storageRef = ref(storage, 'images/' + uploadGambar.name);
+      await uploadBytes(storageRef, uploadGambar);
+
+      // Ambil URL gambar yang diunggah
+      const downloadURL = await getDownloadURL(storageRef);
+
+      // Simpan data di Firestore
+      const docRef = await addDoc(collection(db, "Galeri"), {
+        nama,
+        tahun,
+        downloadURL // Simpan URL gambar
+      });
+      
+      console.log("Document written with ID: ", docRef.id);
+      // Reset form fields after successful submission
+      setUploadGambar("");
+      setNama("");
+      setTahun("");
+      setPreviewImage(""); // Reset preview image
+      setShowSuccessModal(true);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      setShowErrorModal(true);
+      setErrorMessage(error.message);
+    }
+  };
+
+  const handleTahunChange = (e) => {
+    const inputTahun = e.target.value;
+    // Memastikan input tahun hanya berisi angka
+    if (!isNaN(inputTahun)) {
+      setTahun(inputTahun);
+    }
+  };
+
+  const handleUploadGambarChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setUploadGambar(selectedFile);
+      setPreviewImage(URL.createObjectURL(selectedFile)); 
+    }
+  };
+
+  return (
+    <LayoutAdmin>
+      <div>
+        <h2 className="text-lg md:text-2xl font-semibold p-4">Upload Gambar</h2>
+      </div>
+      <div className="flex justify-center items-center p-5 ">
+        <form className="max-w-3xl mx-auto bg-gray-200 shadow-md rounded-lg p-8" onSubmit={handleSubmit}>
+          <div className="grid gap-6 mb-6 md:grid-cols-1">
+            <div>
+              <label
+                htmlFor="uploadGambar"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Upload Gambar
+              </label>
+              <input
+                type="file"
+                id="uploadGambar"
+                onChange={handleUploadGambarChange} // Ubah fungsi pemrosesan gambar
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            {/* Priview gambar */}
+            {previewImage && (
+              <div className="mt-2">
+                <img src={previewImage} alt="Preview" className="w-full h-auto max-h-60 object-cover rounded-lg" />
+              </div>
+            )}
+          </div>
+          
+          <div className="grid gap-6 mb-6 md:grid-cols-1">
+            <div>
+              <label
+                htmlFor="nama"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Nama
+              </label>
+              <input
+                type="text"
+                id="nama"
+                value={nama}
+                onChange={(e) => setNama(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Masukkan Nama"
+                required
+              />
+            </div>
+          </div>
+          <div className="grid gap-6 mb-6 md:grid-cols-1">
+            <div>
+              <label
+                htmlFor="tahun"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Tahun
+              </label>
+              <input
+                type="number"
+                id="tahun"
+                value={tahun}
+                onChange={handleTahunChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Masukkan Tahun"
+                required
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      {/* Success Modal */}
+      {showSuccessModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg">
+            <h2 className="text-lg font-semibold mb-4">Upload berhasil!</h2>
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm ml-1 px-10 py-2.5 "
+              onClick={() => setShowSuccessModal(false)}
+            >
+              Tutup
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Error Modal */}
+      {showErrorModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white rounded-lg overflow-hidden shadow-xl max-w-sm w-full">
+            <div className="p-6">
+              <h2 className="text-lg font-semibold mb-4 text-center text-red-600">Upload gagal!</h2>
+              <p className="text-sm text-gray-700">{errorMessage}</p>
+            </div>
+            <div className="bg-gray-100 p-4 flex justify-center">
+              <button
+                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm ml-2 px-10 py-2.5"
+                onClick={() => setShowErrorModal(false)}
+              >
+                Tutup
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+    </LayoutAdmin>
+  );
+}
+
+export default TambahGaleri;
