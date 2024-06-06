8bda6326e4f0fc560c982a78549ca9fe193cca0e
bendinuralif <bendipenthol@gmail.com>
2024-06-06 10:08:14 +0700

    Card Sejarah

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,6 +4,7 @@ import Intro from "./../components/Intro";
 import CardBerita from "../components/CardBerita";
 import CardKegiatanHome from "../components/CardKegiatanHome";
 import CardBeritaHome from "../components/CardBeritaHome";
+import CardSejarah from "../components/CardSejarah";
 
 function HomePage() {
   return (
@@ -29,38 +30,7 @@ function HomePage() {
                 PERSAUDARAAN SETIA HATI TERATE
               </h5>
             </a>
-            <p class="text-sm mb-3 p-5 font-normal text-gray-700 dark:text-gray-400 md:text-lg text-justify">
-              Didirikan di Madiun pada tahun 1922 oleh Ki Hajar Hardjo Oetomo
-              (1888 – 1952), seorang pahlawan Perintis Kemerdekaan RI, PSHT
-              semula bernama Setia Hati Pemuda Sport Club (SH PSC) yang
-              berbentuk organisasi. Nama ini kemudian menjadi Persaudaraan Setia
-              Hati “Pemuda Sport Club” dan akhirnya diubah menjadi “Persaudaraan
-              Setia Hati Terate” dalam kongres pertama di Madiun, 25 Maret 1951.
-              <br />
-              <br />
-              Perkembangan PSHT tidak terlepas dari jasa beberapa tokoh yang
-              turut membesarkan PSHT, diantaranya Bpk. RM Soetomo Mangkudjojo;
-              Bpk. Santoso Kartoatmodjo; Bpk. Irsyad; Mas RM. Imam Koesoepangat
-              dan Mas KRT Tarmadji Budi Harsono, SE. Beliau-beliaulah yang
-              meletakkan berbagai dasar dan memperintis pengembangan PSHT yang
-              masih digunakan dan berlaku hingga saat ini. Berkat jasa mereka
-              semua, PSHT sejak lama sudah memiliki AD-ART, mendirikan sebuah
-              yayasan, mengembangkan PSHT dengan membentuk banyak cabang,
-              membangun padepokan sebagai pusat kegiatan PSHT, mendirikan
-              koperasi yang kini akan diperluas dengan melibatkan semua anggota
-              di seluruh cabang, dan makin dikenalnya PSHT melalui berbagai
-              kejuaraan.
-              <br />
-              <br />
-              Kepengurusan yang baru di bawah pimpinan DR. Ir. M. Taufiq SH.,
-              MSc. selaku Ketua Umum untuk periode 2021 – 2026, telah menetapkan
-              rencana strategis yang digunakan sebagai pedoman bagi seluruh
-              pengurus di semua tingkatan. Dibandingkan dengan kepengurusan
-              sebelumnya, pada periode ini organisasi PSHT mengalami perubahan
-              struktur dan diperluas dengan menambah bidang Pengabdian
-              Masyarakat untuk lebih mengorganisir kegiatan-kegiatan PSHT yang
-              memberi dampak langsung dan positif terhadap masyarakat.
-            </p>
+            <CardSejarah />
             <a
               href="/sejarah-singkat"
               class="inline-flex items-center px-3 py-2 text-xs  md:text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-5 mb-10"
